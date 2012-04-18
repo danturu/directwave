13d4d644288c8f39b6ebca29f7e9f83c399c724f
Dima Kochnev <kochnev.d@gmail.com>
2012-04-18 12:38:59 +0400

    Moved out from deprecated InstanceMethods idiom

--- lib/directwave/uploader/accreditation.rb
+++ lib/directwave/uploader/accreditation.rb
@@ -4,52 +4,50 @@ module DirectWave
     module Accreditation
       extend ActiveSupport::Concern
       
-      module InstanceMethods
-        ##
-        # === Returns
-        #
-        # [String] Amazon S3 acl
-        #
-        def s3_acl
-          @s3_acl ||= s3_access_policy.to_s.gsub("_", "-")
-        end
+      ##
+      # === Returns
+      #
+      # [String] Amazon S3 acl
+      #
+      def s3_acl
+        @s3_acl ||= s3_access_policy.to_s.gsub("_", "-")
+      end
 
-        ##
-        # === Returns
-        #
-        # [Boolean] Amazon S3 policy
-        #
-        def s3_policy
-          @s3_policy ||= Base64.encode64(
-            {
-              "expiration" => expiration_date,
-              "conditions" => [
-                {"bucket" => s3_bucket},
-                ["starts-with", "$key", upload_dir],
-                {"success_action_status" => "201"},
-                ["starts-with", "$filename", ""],
-                ["starts-with", "$folder", ""],
-                {"acl" => s3_acl},
-                ["content-length-range", 1, max_file_size]
-              ]
-            }.to_json
-          ).gsub("\n","")
-        end
-        
-        ##
-        # === Returns
-        #
-        # [Boolean] Amazon S3 signature
-        #
-        def s3_signature
-          @s3_signature ||= Base64.encode64(
-            OpenSSL::HMAC.digest(
-              OpenSSL::Digest::Digest.new("sha1"), 
-              s3_secret_access_key, s3_policy
-            )
-          ).gsub("\n","")
-        end  
-      end # InstanceMethods
+      ##
+      # === Returns
+      #
+      # [Boolean] Amazon S3 policy
+      #
+      def s3_policy
+        @s3_policy ||= Base64.encode64(
+          {
+            "expiration" => expiration_date,
+            "conditions" => [
+              {"bucket" => s3_bucket},
+              ["starts-with", "$key", upload_dir],
+              {"acl" => s3_acl},
+              {"success_action_status" => "201"},
+              ["content-length-range", 0, max_file_size],
+              ["starts-with", "$Filename", ""],
+              ['starts-with', '#{content_type}', '']
+            ]
+          }.to_json
+        ).gsub("\n","")
+      end
+      
+      ##
+      # === Returns
+      #
+      # [Boolean] Amazon S3 signature
+      #
+      def s3_signature
+        @s3_signature ||= Base64.encode64(
+          OpenSSL::HMAC.digest(
+            OpenSSL::Digest::Digest.new("sha1"), 
+            s3_secret_access_key, s3_policy
+          )
+        ).gsub("\n","")
+      end  
       
       private
       
--- lib/directwave/uploader/paths.rb
+++ lib/directwave/uploader/paths.rb
@@ -4,47 +4,44 @@ module DirectWave
     module Paths
       extend ActiveSupport::Concern
       
-      module InstanceMethods
-        def url(version=:original)
-          versions[version.to_sym].url || default_url
-        end
+      def url(version=:original)
+        versions[version.to_sym].url || default_url
+      end
+      
+      def key(version=:original)
+        versions[version.to_sym].key
+      end
+      
+      def default_url
+        puts "default_url"
+      end
+      
+      def filename(part=nil)
+        return original_filename unless has_store_key?
         
-        def key(version=:original)
-          versions[version.to_sym].key
-        end
-        
-        def default_url
-          puts "default_url"  
-        end
-        
-        def filename(part=nil)
-          return original_filename unless has_store_key?
-          
-          key_path = store_key.split("/")
-          filename_parts = []
-          filename = filename_parts.unshift(key_path.pop)
-          unique_key = key_path.pop
-          filename_parts.unshift(unique_key) if unique_key
-          filename_parts.join("/")
-        end
-                
-        def original_filename
-          model[mounted_as.to_s] if model.respond_to?(mounted_as)
-        end
+        key_path = store_key.split("/")
+        filename_parts = []
+        filename = filename_parts.unshift(key_path.pop)
+        unique_key = key_path.pop
+        filename_parts.unshift(unique_key) if unique_key
+        filename_parts.join("/")
+      end
               
-        def store_key=(string)
-          @store_key = string
-        end
-          
-        def store_key
-          @store_key ||= "#{upload_dir}/#{guid}/${filename}"
-        end
+      def original_filename
+        model[mounted_as.to_s] if model.respond_to?(mounted_as)
+      end
+            
+      def store_key=(string)
+        @store_key = string
+      end
         
-        def has_store_key?
-          @store_key.present? && !(@store_key =~ /#{Regexp.escape("${filename}")}\z/)
-        end
-      end # InstanceMethods
-       
+      def store_key
+        @store_key ||= "#{upload_dir}/#{guid}/${filename}"
+      end
+      
+      def has_store_key?
+        @store_key.present? && !(@store_key =~ /#{Regexp.escape("${filename}")}\z/)
+      end       
     end # Paths
   end # Uploader
   
